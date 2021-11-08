40317842d8baa76f08d487a63ac9f91d4856a34f
Rahul Chopra <scorpio.sk98@gmail.com>
2021-11-08 20:12:13 +0530

    new addition

---
+++ index.js
@@ -0,0 +1,30 @@
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "Bye";
+// headerTitle.innnerHTML = '<h1>Hey</h1>';
+
+
+var header = document.getElementById('main-header');
+header.style.border = 'solid 10px #000';
+
+var title = document.getElementById('main');
+title.style.fontWeigth = 'bold';
+title.style.color = 'red';
+
+
+var items = document.getElementByClassName('list-group-items');
+console.log(items);
+items[2].style.backgroundColor = "green";
+
+for(let i = 0;i<items.length;i++){
+	items[i].style.fontWeigth = 'bold';
+}
+
+
+var li = document.getElementByTagName('li');
+console.log(li);
+li[2].style.backgroundColor = "green";
+
+for(let i = 0;i<li.length;i++){
+	li[i].style.fontWeigth = 'bold';
+}
