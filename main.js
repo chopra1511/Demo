e6c65582fa1512791f8787e92bb89d3c5311ff2a
Rahul Chopra <63909178+chopra1511@users.noreply.github.com>
2021-11-12 13:32:26 +0530

    Add multiple data to localStorage

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
-var frm = document.querySelector('#my-form');
-var nameInput = document.querySelector('#userName');
-var mail = document.querySelector('#userMail');
-var msgInput = document.querySelector('.msg');
-
-frm.addEventListener('submit',onSubmit);
-
-function onSubmit(e) {
-    e.preventDefault();
-    if(nameInput.value === "" || mail.value === ""){
-        msgInput.style.color = 'red';
-        msgInput.textContent = "Please Fill Up!";
-    }
-    setInterval(() => {
-        msgInput.remove();
-    }, 2000);
-
-  let obj = {
-         nameData : nameInput.value,
-         mailData : mail.value
-    }
+document.addEventListener('DOMContentLoaded',function () {
     
-    let newObj = JSON.stringify(obj);
-    localStorage.setItem('obj',newObj);
-    let newObj1 = JSON.parse(localStorage.getItem('obj',newObj));
-    let display = localStorage.getItem('obj',newObj1);
+    var frm = document.querySelector('#my-form');
+    var nameInput = document.querySelector('#userName');
+    var mail = document.querySelector('#userMail');
+    var msgInput = document.querySelector('.msg');
 
-    var items = document.getElementById('items');
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(display));
-    items.appendChild(li);
-    nameInput.value = "";
-    mail.value = "";
+    
 
-}
+
+    frm.addEventListener('submit',onSubmit);
+    displayData();
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if(nameInput.value === "" || mail.value === ""){
+            msgInput.style.color = 'red';
+            msgInput.textContent = "Please Fill Up!";
+        }
+        setInterval(() => {
+             msgInput.remove();
+        }, 2000);
+
+        let formData = JSON.parse(localStorage.getItem('formData')) || [];
+        if(nameInput.value != "" || mail.value != ""){
+            formData.push({
+                nameData : nameInput.value,
+                mailData : mail.value
+            });
+
+            localStorage.setItem('formData', JSON.stringify(formData));
+
+            document.querySelector('#my-form').reset();
+            document.getElementById("userName").focus();
+    
+            displayData();
+        }
+    }
+
+    function displayData() {
+        if(localStorage.getItem('formData')){
+            var output = document.querySelector('#items');
+            output.innerHTML = "";
+            JSON.parse(localStorage.getItem('formData')).forEach( data => {
+                output.innerHTML += `<li>${data.nameData}:${data.mailData}</li>`;
+            });
+        }
+    }
+
+    
+})
